6dc11d3121e7ccba5f85b17ea16f814779c47dc2
George Salu <george.salu10@gmail.com>
2025-02-25 13:30:09 -0300

    primeiros-passos-junit - renomeando SimpleMathTestS3

--- primeiros-passos-junit5/src/test/java/br/com/junit/math/SimpleMathTestS3.java
+++ primeiros-passos-junit5/src/test/java/br/com/junit/math/SimpleMathTestS3.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("teste operacoes em SimpleMath")
-public class SimpleMathTest {
+public class SimpleMathTestS3 {
 
 	SimpleMath math;
 	
