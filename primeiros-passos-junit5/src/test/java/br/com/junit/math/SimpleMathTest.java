b83a855337063b736e81b0060105140f7dc3a0bd
George Salu <george.salu10@gmail.com>
2025-02-25 08:38:05 -0300

    primeiros-passos-junit - definindo a logica do teste unitario e conhecedno as primeiras assertions

---
+++ primeiros-passos-junit5/src/test/java/br/com/junit/math/SimpleMathTest.java
@@ -0,0 +1,18 @@
+package br.com.junit.math;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleMathTest {
+
+	@Test
+	public void testaSoma() {
+		SimpleMath math = new SimpleMath();
+		Double actual = math.sum(6.2D, 2D);
+		double expected = 8.2D;
+		
+		assertEquals(expected, actual);
+	}
+	
+}
