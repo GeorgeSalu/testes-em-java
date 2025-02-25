9bdb1d526127f1815cfcd12c0ec2826eb2e067e8
George Salu <george.salu10@gmail.com>
2025-02-25 14:21:14 -0300

    primeiros-passos-junit - @TestMethodOrder ordenacao por indice com @Order

---
+++ primeiros-passos-junit5/src/test/java/br/com/junit/MethodOrderedByOrderTest.java
@@ -0,0 +1,35 @@
+package br.com.junit;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class MethodOrderedByOrderTest {
+
+	@Test
+	@Order(1)
+	void testeC() {
+		System.out.println(" teste C");
+	}
+	
+	@Test
+	@Order(2)
+	void testeD() {
+		System.out.println(" teste D");
+	}
+	
+	@Test
+	@Order(3)
+	void testeA() {
+		System.out.println(" teste A");
+	}
+	
+	@Test
+	@Order(4)
+	void testeB() {
+		System.out.println(" teste B");
+	}
+	
+}
