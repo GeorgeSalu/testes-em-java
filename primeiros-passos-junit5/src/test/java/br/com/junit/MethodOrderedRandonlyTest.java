0616963e47d70ad5f954e0d59fd3612d6540ae3b
George Salu <george.salu10@gmail.com>
2025-02-25 14:14:46 -0300

    primeiros-passos-junit - @TestMethodOrder ordenacao randomica

---
+++ primeiros-passos-junit5/src/test/java/br/com/junit/MethodOrderedRandonlyTest.java
@@ -0,0 +1,30 @@
+package br.com.junit;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+class MethodOrderedRandonlyTest {
+
+	@Test
+	void testeA() {
+		System.out.println(" teste A");
+	}
+	
+	@Test
+	void testeB() {
+		System.out.println(" teste B");
+	}
+	
+	@Test
+	void testeC() {
+		System.out.println(" teste C");
+	}
+	
+	@Test
+	void testeD() {
+		System.out.println(" teste D");
+	}
+
+}
