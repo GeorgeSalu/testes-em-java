be4ae4602aac5dcac4eab71c614c22dd61eca071
George Salu <george.salu10@gmail.com>
2025-02-25 13:11:39 -0300

    primeiros-passos-junit - testando perfomance

--- primeiros-passos-junit5/src/test/java/br/com/junit/ArraysCompareTest.java
+++ primeiros-passos-junit5/src/test/java/br/com/junit/ArraysCompareTest.java
@@ -3,8 +3,10 @@ package br.com.junit;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 class ArraysCompareTest {
 
@@ -17,6 +19,20 @@ class ArraysCompareTest {
 		Arrays.sort(numbers);
 	
 		assertArrayEquals(numbers, expectedArray);
+	}
+	
+	@Test
+	// @Timeout(1)
+	@Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+	public void testSortPerformance() {
+	
+		int[] numbers = {25, 8, 21, 32, 3};
+		
+		for(int i = 0;i < 1000000000; i++) {
+			numbers[0] = i;
+			Arrays.sort(numbers);
+		}
+	
 	}
 
 }
