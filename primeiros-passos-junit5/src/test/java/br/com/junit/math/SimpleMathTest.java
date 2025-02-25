7d9ff193ae467454446fc52763368b27e05b90d4
George Salu <george.salu10@gmail.com>
2025-02-25 08:55:26 -0300

    primeiros-passos-junit - implementando testes automatizados para as demais operacoes

--- primeiros-passos-junit5/src/test/java/br/com/junit/math/SimpleMathTest.java
+++ primeiros-passos-junit5/src/test/java/br/com/junit/math/SimpleMathTest.java
@@ -15,9 +15,56 @@ public class SimpleMathTest {
 		Double actual = math.sum(firstNumber, secondNumber);
 		double expected = 8.2D;
 		
-		assertEquals(expected, actual, () -> firstNumber +" + "+secondNumber+" nao resultou em 8.2");
+		assertEquals(expected, actual, () -> firstNumber +" + "+secondNumber+" nao resultou em "+expected+"");
 		assertNotEquals(9.2, actual);
 		assertNotNull(actual);
+	}
+	
+	@Test
+	public void testaSubtracao() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.subtraction(firstNumber, secondNumber);
+		double expected = 4.2D;
+		
+		assertEquals(expected, actual, () -> firstNumber +" + "+secondNumber+" nao resultou em "+expected+"");
+	}
+	
+	@Test
+	public void testaMultiplicacao() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.multiplication(firstNumber, secondNumber);
+		double expected = 12.4D;
+		
+		assertEquals(expected, actual, () -> firstNumber +" + "+secondNumber+" nao resultou em "+expected+"");
+	}
+	
+	@Test
+	public void testaDivisao() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.division(firstNumber, secondNumber);
+		double expected = 3.1D;
+		
+		assertEquals(expected, actual, () -> firstNumber +" + "+secondNumber+" nao resultou em "+expected+"");
+	}
+	
+	@Test
+	public void testaMedia() {
+		SimpleMath math = new SimpleMath();
+		double number = 81D;
+		
+		Double actual = math.squareRoot(number);
+		double expected = 9D;
+		
+		assertEquals(expected, actual);
 	}
 	
 }
