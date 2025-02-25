8aae7042ac05f1f098629b2c6f8917b1b2fe0e0b
George Salu <george.salu10@gmail.com>
2025-02-25 15:32:20 -0300

    primeiros-passos-tdd - criando o teste de criacao de pessoa

---
+++ primeiros-passos-tdd/src/test/java/br/com/tdd/PersonServiceTest.java
@@ -0,0 +1,18 @@
+package br.com.tdd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+	@DisplayName("quando criar uma pessoa com sucesso deve retornar um objeto de pessoa")
+	@Test
+	public void testeQuandoCriarUmaPessoaDeveRetornarUmObjetoDePessoa() {
+		// Given / Arrange
+		
+		// When / Act
+		
+		// Then / Assert
+	}
+	
+}
