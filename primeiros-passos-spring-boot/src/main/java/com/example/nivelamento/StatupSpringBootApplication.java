f7195a33c84ae3640b08a3090ab400c8a88fc3ca
George Salu <george.salu10@gmail.com>
2025-02-26 15:54:30 -0300

    criando o projeto - primeiros-passos-spring-boot

---
+++ primeiros-passos-spring-boot/src/main/java/com/example/nivelamento/StatupSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.nivelamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatupSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatupSpringBootApplication.class, args);
+	}
+
+}
---
+++ primeiros-passos-spring-boot/src/test/java/com/example/nivelamento/StatupSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nivelamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatupSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
