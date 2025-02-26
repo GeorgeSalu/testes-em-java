134a8a1bd94e7bc6b3c2a420d09515af74666fa1
George Salu <george.salu10@gmail.com>
2025-02-26 16:33:50 -0300

    primeiros-passos-spring-boot - remove referencia de AtomicLong que nao e mas usada

--- primeiros-passos-spring-boot/src/main/java/com/example/nivelamento/MathController.java
+++ primeiros-passos-spring-boot/src/main/java/com/example/nivelamento/MathController.java
@@ -1,7 +1,5 @@
 package com.example.nivelamento;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 
-	private final AtomicLong counter = new AtomicLong();
-	
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sum(
 			@PathVariable(value = "numberOne") String numberOne,
