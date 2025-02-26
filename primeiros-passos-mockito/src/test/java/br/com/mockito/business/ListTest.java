b2ef0145434b98bac62196c1bc12218af3864c16
George Salu <george.salu10@gmail.com>
2025-02-26 10:37:16 -0300

    primeiros-passos-mockito - mockando exceptions

--- primeiros-passos-mockito/src/test/java/br/com/mockito/business/ListTest.java
+++ primeiros-passos-mockito/src/test/java/br/com/mockito/business/ListTest.java
@@ -1,6 +1,7 @@
 package br.com.mockito.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
@@ -20,4 +21,20 @@ public class ListTest {
 		assertEquals(10, list.size());
 	}
 	
+	
+	@Test
+	public void testMockList_Quando_RetornarException() {
+		// Given / Arrange
+		var list = mock(List.class);
+		
+		// matchers
+		when(list.get(anyInt())).thenThrow(new RuntimeException("Foo Bar"));
+		
+		// When / Act
+		// Then / Assert
+		assertThrows(RuntimeException.class, 
+				() -> {
+					list.get(anyInt());
+				}, () -> "tem que retornar um RuntimeException");
+	}
 }
