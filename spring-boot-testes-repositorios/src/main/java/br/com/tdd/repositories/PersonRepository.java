ee18ecc87a0fa05f10bb21db40972477c4c16e55
George Salu <george.salu10@gmail.com>
2025-02-27 11:49:50 -0300

    cria projeto - spring-boot-testes-repositorios

--- spring-boot-testes-repositorios/src/main/java/br/com/tdd/repositories/PersonRepository.java
+++ spring-boot-testes-repositorios/src/main/java/br/com/tdd/repositories/PersonRepository.java
@@ -1,13 +1,11 @@
 package br.com.tdd.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.tdd.model.Person;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    
-    Optional<Person> findByEmail(String email);
     
 }
