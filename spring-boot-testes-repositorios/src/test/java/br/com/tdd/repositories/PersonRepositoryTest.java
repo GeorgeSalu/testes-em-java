1d3c7a3758ec7395da7d4ecb9a0c85cda73865e5
George Salu <george.salu10@gmail.com>
2025-02-27 14:18:20 -0300

    spring-boot-testes-repositorios - testando a operacao responsavel por listar pessoas

--- spring-boot-testes-repositorios/src/test/java/br/com/tdd/repositories/PersonRepositoryTest.java
+++ spring-boot-testes-repositorios/src/test/java/br/com/tdd/repositories/PersonRepositoryTest.java
@@ -36,7 +36,7 @@ class PersonRepositoryTest {
 	
 	@DisplayName("quando invocar findAll do repositorio de pessoas deve retornar uma lista de pessoas")
 	@Test
-	public void testaFindAllDePErson() {
+	public void testaFindAllDePerson() {
 		// Given / Arrange
 		Person person0 = new Person("george", "silva", "george@gmail.com", "belem", "m");
 		Person person1 = new Person("george", "silva", "george@gmail.com", "belem", "m");
