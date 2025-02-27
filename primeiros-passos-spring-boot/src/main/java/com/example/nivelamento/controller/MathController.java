b78bd5e7694fc7c409e874bc88715c2210fd95fa
George Salu <george.salu10@gmail.com>
2025-02-27 08:43:14 -0300

    primeiros-passos-spring-boot - refatoracao criando pasta controller

--- primeiros-passos-spring-boot/src/main/java/com/example/nivelamento/controller/MathController.java
+++ primeiros-passos-spring-boot/src/main/java/com/example/nivelamento/controller/MathController.java
@@ -1,4 +1,4 @@
-package com.example.nivelamento;
+package com.example.nivelamento.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
