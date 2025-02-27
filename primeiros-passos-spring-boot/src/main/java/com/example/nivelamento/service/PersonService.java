a9e744cf075f57c92089ae1ce27c3d7ec76cdd85
George Salu <george.salu10@gmail.com>
2025-02-27 09:18:48 -0300

    primeiros-passos-spring-boot - retornando lista de objetos json

--- primeiros-passos-spring-boot/src/main/java/com/example/nivelamento/service/PersonService.java
+++ primeiros-passos-spring-boot/src/main/java/com/example/nivelamento/service/PersonService.java
@@ -1,5 +1,7 @@
 package com.example.nivelamento.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,18 @@ public class PersonService {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
 	
+	public List<Person> findAll() {
+		
+		List<Person> persons = new ArrayList<Person>();
+		
+		for(int i = 0;i < 8;i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+
 	public Person findById(String id) {
 		
 		logger.info("find one person");
@@ -20,6 +34,19 @@ public class PersonService {
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("george");
 		person.setLastName("silva");
+		person.setAddress("belem");
+		person.setGender("masculino");
+		
+		return person;
+	}
+
+
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("perons name "+i);
+		person.setLastName("last name "+i);
 		person.setAddress("belem");
 		person.setGender("masculino");
 		
