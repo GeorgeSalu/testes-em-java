1b039e2e2ac195e1216b743d97d56c570f509fed
George Salu <george.salu10@gmail.com>
2025-03-03 09:29:34 -0300

    spring-boot-testes-repositorios - testando a operacao responsavel por consulta de pessoa por firstName e lastName usando sql nativo e named parameters

--- spring-boot-testes-repositorios/src/test/java/br/com/tdd/repositories/PersonRepositoryTest.java
+++ spring-boot-testes-repositorios/src/test/java/br/com/tdd/repositories/PersonRepositoryTest.java
@@ -173,5 +173,22 @@ class PersonRepositoryTest {
 		assertEquals("george", savedPerson.getFirstName());
 		assertEquals("silva", savedPerson.getLastName());
 	}
+	
+	@DisplayName("operacao responsavel por buscar pessoa por firstName e lastName usando sql nativo e named parameters")
+	@Test
+	public void testaOperacaoResponsavelPorBuscarPessoaPorFirstNameELastNameUsandoSQLNativoENamedParameters() {
+		// Given / Arrange
+		Person person0 = new Person("george", "silva", "george@gmail.com", "belem", "m");
+
+		repository.save(person0);
+		
+		// When / Act
+		Person savedPerson = repository.findByNativeSQLNamedParameters("george", "silva");
+		
+		// Then / Assert
+		assertNotNull(person0);
+		assertEquals("george", savedPerson.getFirstName());
+		assertEquals("silva", savedPerson.getLastName());
+	}
 
 }
