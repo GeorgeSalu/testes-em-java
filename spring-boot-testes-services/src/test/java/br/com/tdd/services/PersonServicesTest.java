45d15b5f2c6abdba7326157225f54cca792dfd90
George Salu <george.salu10@gmail.com>
2025-03-03 11:38:40 -0300

    spring-boot-testes-services - organizando imports

--- spring-boot-testes-services/src/test/java/br/com/tdd/services/PersonServicesTest.java
+++ spring-boot-testes-services/src/test/java/br/com/tdd/services/PersonServicesTest.java
@@ -1,10 +1,7 @@
 package br.com.tdd.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
