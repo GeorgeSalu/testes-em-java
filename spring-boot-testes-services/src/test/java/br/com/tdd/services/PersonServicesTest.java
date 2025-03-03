de79e9d08c85160cf8c26473895e94774b7764c2
George Salu <george.salu10@gmail.com>
2025-03-03 11:36:58 -0300

    spring-boot-testes-services - testando o metodo save de PersonService

---
+++ spring-boot-testes-services/src/test/java/br/com/tdd/services/PersonServicesTest.java
@@ -0,0 +1,57 @@
+package br.com.tdd.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.tdd.model.Person;
+import br.com.tdd.repositories.PersonRepository;
+
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServicesTest {
+
+	@Mock
+	private PersonRepository repository;
+	
+	@InjectMocks
+	private PersonServices service;
+	
+	private Person person;
+	
+	@BeforeEach
+	public void setup() {
+		person = new Person("george", "silva", "george@gmail.com", "belem", "m");
+	}
+	
+	@DisplayName("valida o cadastro de person")
+	@Test
+	public void testaCadastroDePessoa() {
+		// Given / Arrange
+		given(repository.findByEmail(anyString())).willReturn(Optional.empty());
+		given(repository.save(person)).willReturn(person);
+		
+		// When / Act
+		Person savedPErson = service.create(person);
+		
+		// Then / Assert
+		assertNotNull(savedPErson);
+		assertEquals("george", savedPErson.getFirstName());
+	}
+
+}
+
+
