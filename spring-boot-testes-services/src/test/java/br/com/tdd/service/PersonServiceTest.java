90c092fe56771534ea5bcb2da58187500a565919
George Salu <george.salu10@gmail.com>
2025-03-04 19:28:48 -0300

    spring-boot-testes-services - melhora nome do teste unitario de cadastro de pessoa

--- spring-boot-testes-services/src/test/java/br/com/tdd/service/PersonServiceTest.java
+++ spring-boot-testes-services/src/test/java/br/com/tdd/service/PersonServiceTest.java
@@ -38,7 +38,7 @@ public class PersonServiceTest {
 	
 	@DisplayName("valida o cadastro de pessoa com sucesso")
 	@Test
-	public void validaCadastroDePessoaComSucessoDeveRetornarOObjetoCadastrado() {
+	public void valida_CadastroDePessoaComSucesso_DeveRetornarOObjetoCadastrado() {
 		// Given / Arrange
 		given(repository.findByEmail(anyString())).willReturn(Optional.empty());
 		given(repository.save(person)).willReturn(person);
