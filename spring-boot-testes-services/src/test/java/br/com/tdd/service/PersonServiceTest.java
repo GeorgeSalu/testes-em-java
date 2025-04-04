f56200ff0009705d68f6d9172e9f34152c5c4c96
George Salu <george.salu10@gmail.com>
2025-04-04 12:50:52 -0300

    spring-boot-testes-services - testando o metodo findAll de PersonService - cenario negativo

--- spring-boot-testes-services/src/test/java/br/com/tdd/service/PersonServiceTest.java
+++ spring-boot-testes-services/src/test/java/br/com/tdd/service/PersonServiceTest.java
@@ -68,7 +68,7 @@ public class PersonServiceTest {
 		assertEquals("Person already exist with given email: "+person.getEmail(), ex.getMessage());
 	}
 	
-	@DisplayName("valida a lista de pessoas fornecidade pelo metodo findAll() que deve retornar todas as pessoas")
+	@DisplayName("valida a lista de pessoas fornecidade pelo metodo findAll() que deve retornar todas as pessoas existente na base")
 	@Test
 	public void valida_ListagemDePessoasFornecidoPorFindAll_DeveRetornarTodasAsPessoasCadastradas() {
 		// Given / Arrange
