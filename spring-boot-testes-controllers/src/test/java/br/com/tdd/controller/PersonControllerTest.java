684d45d601d0d1bff0203c157bbc98e3aebee6cd
George Salu <george.salu10@gmail.com>
2025-04-07 16:09:16 -0300

    spring-boot-testes-controllers - testando a operacao de create

--- spring-boot-testes-controllers/src/test/java/br/com/tdd/controller/PersonControllerTest.java
+++ spring-boot-testes-controllers/src/test/java/br/com/tdd/controller/PersonControllerTest.java
@@ -1,11 +1,29 @@
 package br.com.tdd.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.tdd.model.Person;
+import br.com.tdd.service.PersonService;
+
+import static org.mockito.BDDMockito.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @WebMvcTest
 public class PersonControllerTest {
@@ -13,4 +31,40 @@ public class PersonControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private ObjectMapper mapper;
+	
+	@MockBean
+	private PersonService service;
+	
+	private Person person;
+	
+	@BeforeEach
+	public void setup() {
+		// given / arrange
+		person = new Person("george", "silva", "george@gmail.com", "belem", "m");
+	}
+	
+	@DisplayName("testando a operacao de create")
+	@Test
+	public void valida_OperacaoDeCreate_DeveRetornarOObjetoSalvo() throws JsonProcessingException, Exception {
+		// Given / Arrange
+		given(service.create(any(Person.class))).willAnswer((invocation) -> invocation.getArgument(0));
+
+		// When / Act
+		ResultActions response = mockMvc.perform(post("/person")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(person)));
+
+		// Then / Assert
+		response.andDo(print()).
+			andExpect(status().isOk())
+			.andExpect(jsonPath("$.firstName", is(person.getFirstName())))
+			.andExpect(jsonPath("$.firstName", is(person.getFirstName())))
+			.andExpect(jsonPath("$.firstName", is(person.getFirstName())));
+	}
+	
 }
+
+
+
