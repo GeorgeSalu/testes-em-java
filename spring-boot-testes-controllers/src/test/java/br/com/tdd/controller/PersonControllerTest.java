56d4d7c6d9c5887879b54693e4dd4a44c2f3f1dd
George Salu <george.salu10@gmail.com>
2025-04-07 13:23:45 -0300

    spring-boot-testes-controllers - estrutura incial do PersonControllerTest

---
+++ spring-boot-testes-controllers/src/test/java/br/com/tdd/controller/PersonControllerTest.java
@@ -0,0 +1,16 @@
+package br.com.tdd.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class PersonControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+}
