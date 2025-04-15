fd0133095581356d2174a6f8ff0f4b8946015517
George Salu <george.salu10@gmail.com>
2025-04-15 14:26:34 -0300

    spring-boot-testes-integracao - preparando a infraestrutura de testes de integracao para os endpoints de PersonController

--- spring-boot-testes-integracao/src/test/java/br/com/tdd/controller/integracao/PersonControllerTest.java
+++ spring-boot-testes-integracao/src/test/java/br/com/tdd/controller/integracao/PersonControllerTest.java
@@ -2,17 +2,48 @@ package br.com.tdd.controller.integracao;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.tdd.config.TestConfigs;
+import br.com.tdd.model.Person;
 import br.com.tdd.testcontainers.AbstractTesteIntegracao;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
 
 @TestMethodOrder(OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class PersonControllerTest extends AbstractTesteIntegracao {
 
+	private static RequestSpecification specification;
+	private static ObjectMapper objectMapper;
+	private static Person person;
+	
+	@BeforeAll
+	public void setup() {
+		objectMapper = new ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		
+		specification = new RequestSpecBuilder()
+								.setBasePath("")
+								.setPort(TestConfigs.SERVER_PORT)
+								.addFilter(new RequestLoggingFilter(LogDetail.ALL))
+								.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+								.build();
+		
+		person = new Person("george", "silva", "george@gmail.com", "belem", "m");
+	}
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
