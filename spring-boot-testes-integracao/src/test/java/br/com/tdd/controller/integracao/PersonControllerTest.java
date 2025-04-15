ad35076bdc142c3c215fd40ee4578c420aaa29aa
George Salu <george.salu10@gmail.com>
2025-04-15 15:44:31 -0300

    spring-boot-testes-integracao - testando a operacao de create

--- spring-boot-testes-integracao/src/test/java/br/com/tdd/controller/integracao/PersonControllerTest.java
+++ spring-boot-testes-integracao/src/test/java/br/com/tdd/controller/integracao/PersonControllerTest.java
@@ -1,14 +1,21 @@
 package br.com.tdd.controller.integracao;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.tdd.config.TestConfigs;
@@ -29,18 +36,48 @@ class PersonControllerTest extends AbstractTesteIntegracao {
 	private static Person person;
 	
 	@BeforeAll
-	public void setup() {
+	public static void setup() {
 		objectMapper = new ObjectMapper();
 		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 		
 		specification = new RequestSpecBuilder()
-								.setBasePath("")
+								.setBasePath("/person")
 								.setPort(TestConfigs.SERVER_PORT)
 								.addFilter(new RequestLoggingFilter(LogDetail.ALL))
 								.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
 								.build();
 		
 		person = new Person("george", "silva", "george@gmail.com", "belem", "m");
+	}
+	
+	
+	@Test
+	@Order(1)
+	@DisplayName("Junit integration given Person Object when create one Person should return a person object")
+	public void integrationTest_when_createOnePerson_shouldReturnAPersonObject() throws JsonMappingException, JsonProcessingException {
+		var content = given().spec(specification)
+					.contentType(TestConfigs.CONTENT_TYPE_JSON)
+					.body(person)
+	            .when()
+	                .post()
+	            .then()
+	                .statusCode(200)
+			            .extract()
+			                .body()
+			                    .asString();
+		
+		Person createdPerson = objectMapper.readValue(content, Person.class);
+		
+		person = createdPerson;
+		
+		assertNotNull(createdPerson);
+		assertNotNull(createdPerson.getId());
+		assertNotNull(createdPerson.getFirstName());
+		assertNotNull(createdPerson.getLastName());
+		assertNotNull(createdPerson.getAddress());
+		assertNotNull(createdPerson.getGender());
+		assertNotNull(createdPerson.getEmail());
+
 	}
 	
 
