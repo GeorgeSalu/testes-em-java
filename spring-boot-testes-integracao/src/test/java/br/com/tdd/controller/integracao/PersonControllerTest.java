7aebe34858c5aef272a481193b086f8cf5608fef
George Salu <george.salu10@gmail.com>
2025-04-15 12:23:30 -0300

    spring-boot-testes-integracao - preparando a infraestrutura de testes de integracao para os endpoints de PersonController

--- spring-boot-testes-integracao/src/test/java/br/com/tdd/controller/integracao/PersonControllerTest.java
+++ spring-boot-testes-integracao/src/test/java/br/com/tdd/controller/integracao/PersonControllerTest.java
@@ -1,13 +1,15 @@
 package br.com.tdd.controller.integracao;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.tdd.testcontainers.AbstractTesteIntegracao;
 
+@TestMethodOrder(OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class PersonControllerTest extends AbstractTesteIntegracao {
 
